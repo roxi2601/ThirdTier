a14f0fefdb170c4841b4aa24efe3b9dadc3612bb
fanimator12 <torok.franciska.leonora@gmail.com>
2020-11-08 17:54:23 +0100

    update serverside

--- server/src/Server.java
+++ server/src/Server.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.rmi.Remote;
@@ -6,6 +8,7 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Scanner;
 
 public class Server {
 	private ServerSocket welcomeSocket;
@@ -17,24 +20,37 @@ public class Server {
 		ServerSocket welcomeSocket = new ServerSocket(port);
 	}
 
-	public void execute() throws IOException {
-		while (true){
-			System.out.println("Waiting for a client...");
-			//Wait, on welcoming socket for contact by client
-			Socket socket = welcomeSocket.accept();
+	public static void main(String[] args)
+			throws IOException, ClassNotFoundException {
+		Scanner keyboard = new Scanner(System.in);
+		while (true) {
+			Socket socket = new Socket("localhost", 1099);
 
-			//Start a thread with the client communication
-			/*TODO !!!!
-			Thread clientThread = new Thread(new CommunicationThreadHandler(socket));
-			clientThread.start();*/
+			ObjectOutputStream outToServer = new ObjectOutputStream(socket.getOutputStream());
+			ObjectInputStream inFromSever = new ObjectInputStream(socket.getInputStream());
+
+			String connection = keyboard.nextLine();
+			outToServer.writeObject(connection);
+
+			System.out.println((String) inFromSever.readObject());
+
+			String username = keyboard.nextLine();
+			outToServer.writeObject(username);
+
+			System.out.println((String) inFromSever.readObject());
+
+			String password = keyboard.nextLine();
+			outToServer.writeObject(password);
+
+			System.out.println((String) inFromSever.readObject());
 		}
 	}
 
-	public static void main(String[] args) throws RemoteException {
+	/*public static void main(String[] args) throws RemoteException {
 		RemoteBase base = new RemoteBase(DAOLocator.getDAO());
 		Remote skeleton = UnicastRemoteObject.exportObject(base, 8080);
 		Registry registry = LocateRegistry.getRegistry(1099);
 		registry.rebind("Base", skeleton);
 		System.out.println("Server running");
-	}
+	}*/
 }
